d0c5b360f097aab4676c128b85c088ca720eeecb
y elardy <yomna.a.elardy@gmail.com>
2018-05-11 19:13:28 +0200

    retrieve

--- sw_maintenance/src/sw_maintenance/DatabaseEntity.java
+++ sw_maintenance/src/sw_maintenance/DatabaseEntity.java
@@ -12,93 +12,98 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-
 public class DatabaseEntity {
+
     private String databaseName, userName, password;
-    private Connection connection; 
-    private String      url;
+    private Connection connection;
+    private String url;
     private Statement statment;
-    private DBSet  Dbset;
-   public DatabaseEntity(){}
-    public DatabaseEntity(String databaseName, String userName, String password){
+    private DBSet Dbset;
+
+    public DatabaseEntity() {
+    }
+
+    public DatabaseEntity(String databaseName, String userName, String password) {
         this.databaseName = databaseName;
         this.userName = userName;
         this.password = password;
-        System.out.println(this.databaseName+" "+this.userName+" "+this.password);
+        System.out.println(this.databaseName + " " + this.userName + " " + this.password);
     }
-    public DBSet getDBInstance(Class<?> className){
-      
-        DBSet db=new DBSet(this,className); 
-        this.Dbset=db;
-       // this.Dbset.DBInitation(className);
+
+    public DBSet getDBInstance(Class<?> className) {
+
+        DBSet db = new DBSet(this, className);
+        this.Dbset = db;
+        // this.Dbset.DBInitation(className);
         return db;
- 
+
     }
-    public void openConnection(){
+
+    public void openConnection() {
         System.out.println("Test for entering the function ");
-        try{
-            
+        try {
+
             //Register JDBC driver
             Class.forName("com.mysql.jdbc.Driver");
-            url="jdbc:mysql://localhost:3306/";
+            url = "jdbc:mysql://localhost:3306/";
             //Open a connection
-            connection = DriverManager.getConnection(  
-            url, userName, password);
-               System.out.println("Connected");
+            connection = DriverManager.getConnection(
+                    url, userName, password);
+            System.out.println("Connected");
             //Execute a query
             statment = connection.createStatement();
             String sql = "CREATE DATABASE " + databaseName;
-            System.out.println("Dataabse name "+databaseName);
+            System.out.println("Dataabse name " + databaseName);
             statment.executeUpdate(sql);
-            url="jdbc:mysql://localhost:3306/"+databaseName;
-            connection = DriverManager.getConnection(  
-            url, userName, password);
-           
+            url = "jdbc:mysql://localhost:3306/" + databaseName;
+            connection = DriverManager.getConnection(
+                    url, userName, password);
+
+        } catch (SQLException e) {
+            System.out.println("Error in opening " + e);
+
+            try {
+                System.out.println("Database is already existed");
+                url = "jdbc:mysql://localhost:3306/" + databaseName.toLowerCase();
+                //Open a connection
+                connection = DriverManager.getConnection(url, userName, password);
+                //Execute a query
+                
+                statment = connection.createStatement();
+                System.out.println("Execute a query");
+            } catch (SQLException ex) {
+                System.out.println("Error  " + ex);
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error  " + e);
         }
-        catch(SQLException e){
-        System.out.println("Error  "+e);
-        
-        try{
-            System.out.println("Database is already existed");
-            url="jdbc:mysql://localhost:3306/"+databaseName.toLowerCase();
-            //Open a connection
-        connection = DriverManager.getConnection(  url, userName, password);
-           //Execute a query
-            statment = connection.createStatement();
-        }
-        
-        catch(SQLException ex){System.out.println("Error  "+ex);}
-        }
-       
-        catch(ClassNotFoundException e){
-        System.out.println("Error  "+e);
     }
-    
-    }
-    
-    public void closeConnection(){
-        try{
+
+    public void closeConnection() {
+        try {
             connection.close();
             statment.close();
             System.out.println("Connection is closed ");
-        }catch(SQLException e){}
+        } catch (SQLException e) {
+        }
     }
-    
-    public void excuteSql(String query) throws SQLException{
-        System.out.println("Query: "+query);
-        
-        try{
-       int i= statment.executeUpdate(query);
-      
-        System.out.println("result of query "+i);}
-        catch(Exception e){
+
+    public void excuteSql(String query) throws SQLException {
+        System.out.println("Query: " + query);
+
+        try {
+            int i = statment.executeUpdate(query);
+
+            System.out.println("result of query " + i);
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
+
     // New added by Team one
-     public ResultSet excuteQUERY(String query) throws SQLException{
-         System.out.println("Execute Query "+query);
-         System.out.println(" "+statment.executeQuery(query));
+    public ResultSet excuteQUERY(String query) throws SQLException {
+        System.out.println("Execute Query " + query);
+        System.out.println(" " + statment.executeQuery(query));
         return statment.executeQuery(query);
     }
 }
--- sw_maintenance/src/sw_maintenance/Sw_maintenance.java
+++ sw_maintenance/src/sw_maintenance/Sw_maintenance.java
@@ -5,6 +5,7 @@
  */
 package sw_maintenance;
 
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -18,18 +19,92 @@ public class Sw_maintenance {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SQLException {
-        user u=new user("DBTest","root","");
-   
-        u.openConnection();
-        DBSet<user> u1= u.getDBInstance(user.class);
+        testingClass u = new testingClass("mysql", "root", "Manosazzz1271995");
+        u.setid(2);
+        u.setnn("testing");
+        u.setFlNumber(10.95f);
+        u.setFlag(false);
+        u.setC('o');
+        u.setLl(125647855);
+        u.setD(4.46);
+//        
         
-        ArrayList<user> arr=u.getDBInstance(user.class).findBy("id", 1);
-        ArrayList<user> arr2=u.getDBInstance(user.class).findAll();
-        System.out.println("Array size "+arr.size());
-       System.out.println("value of id for first row is  "+arr.get(0).getid());
-        System.out.println("value of second field nn for firs row is  "+arr2.get(0).getnn());
-        System.out.println("value of second field nn for second row is : "+arr2.get(1).getnn());
+        
+        
+        u.openConnection();
+        
+        
+        DBSet<testingClass> u1 = u.getDBInstance(testingClass.class);
+
+        u1.add(u);
+
+        
+        ArrayList<testingClass> arr = u.getDBInstance(testingClass.class).findBy("id", 1);
+        
+        testingClass object = (testingClass)u.getDBInstance(testingClass.class).findById("id", 1);
+        
+        ArrayList<testingClass> arr2 = u.getDBInstance(testingClass.class).findAll();
+        System.out.println("Array size " + arr.size());
+        
+        System.out.println("");
+        System.out.println("");
+        System.out.println("data with id 1");
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.println("member " + i);
+            printData(arr.get(i));
+            
+        }
+        System.out.println("");
+        System.out.println("");
+        
+//        
+//        if (arr.size() > 0) {
+//            System.out.println("value of id for first row is  " + arr.get(0).getid());
+//            
+//        }else{
+//            System.out.println("array lis is empty");
+//        }
+        
+        System.out.println("");
+        System.out.println("");
+        System.out.println("all data in database");
+        for (int i = 0; i < arr2.size(); i++) {
+            System.out.println("member " + i);
+            printData(arr2.get(i));
+            
+        }
+        System.out.println("");
+        System.out.println("");
+        
+        
+        System.out.println("");
+        System.out.println("");
+        System.out.println("findby function which returns object");
+        if(object != null){
+            printData(object);
+        }else{
+            System.out.println("no data returned");
+        }
+//        System.out.println("value of field nn for firs row is  " + "String is" + object.getnn() + "\n"  + "\n" + "integer is" + object.getid());
+        
+
+        System.out.println("");
+        System.out.println("");
+//            System.out.println("value of second field nn for second row is : " + arr2.get(1).getnn());
+            
+            
         u.closeConnection();
     }
     
+    static void printData(testingClass u){
+        String s = u.getnn();
+        int jj = u.getid();
+        char cc = u.getC();
+        boolean bb = u.isFlag();
+        long ll = u.getLl();
+        float f = u.getFlNumber();
+        double d = u.getD();
+        System.out.println("text is " + s + " number is " + jj + " charcater is " + cc + " boolean is " + bb + " double number is " + d + " float number is " + f);
+    }
+
 }
